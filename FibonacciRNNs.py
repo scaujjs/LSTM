ad9aa6828256aad6dda10036a758a9a54124d716
scaujjs <648847278@qq.com>
2017-06-08 19:01:24 +0100

    there are but in the fibonacciRNNs

---
+++ FibonacciRNNs.py
@@ -0,0 +1,132 @@
+import copy, numpy as np
+
+def sigmoid(x):
+    return 1/(1+np.exp(-x))
+
+def sigmoid_otput_to_derivation(output):
+    return output*(1-output)
+
+binary_dim=8
+
+largest_number=pow(2,binary_dim)
+
+def int2binary(num):
+    a = bin(num)
+    a = a[2:len(a)]
+    num_bin = np.zeros(binary_dim)
+
+    for i in range(binary_dim - len(a), binary_dim):
+        num_bin[i] = int(a[i + len(a) - binary_dim])
+    return num_bin
+
+def fibonacci(num):
+    number=np.zeros(binary_dim)
+    for i in range(binary_dim):
+
+          number[i]=foundFib(num+i)
+    return number/2000
+
+
+def foundFib(num):
+    if (num==0):
+        return 0
+    elif(num==1):
+        return 1
+    else:
+        return foundFib(num-1)+foundFib(num-2)
+
+
+
+
+alpha=0.5
+input_dim=1
+hidden_dim=16
+output_dim=1
+
+synapse_0=0.1*np.random.random((input_dim,hidden_dim))-0.05
+synapse_1=0.1*np.random.random((hidden_dim,output_dim))-0.05
+synapse_h=0.1*np.random.random((hidden_dim,hidden_dim))-0.05
+
+synapse_0_update=np.zeros_like(synapse_0)
+synapse_1_update=np.zeros_like(synapse_1)
+synapse_h_update=np.zeros_like(synapse_h)
+
+
+for i in range(100000):
+    number=np.random.randint(10)
+    num=fibonacci(number)
+
+
+    overallError = 0
+
+    layer_2_deltas = list()
+    layer_1_values = list()
+    layer_1_values.append(sigmoid(np.dot(num[0], synapse_0)))
+
+    result=np.zeros(6)
+
+    X= np.array([[num[1]]])
+    for position in range(6):
+        Y = np.array([[num[position+2]]]).T
+
+        layer_1 = sigmoid(np.dot(X, synapse_0) + np.dot(layer_1_values[-1], synapse_h))
+        layer_2 = sigmoid(np.dot(layer_1, synapse_1))
+
+        np.dot(layer_1, synapse_1)
+        X[0][0]=layer_2[0][0]
+        layer_2_error = Y - layer_2
+        layer_2_deltas.append(layer_2_error * sigmoid_otput_to_derivation(layer_2))
+
+
+        overallError += np.abs(layer_2_error[0][0])
+
+
+        result[position]=(layer_2[0][0])
+       # d[binary_dim-1-position] = np.round(layer_2[0][0])
+
+        layer_1_values.append(copy.deepcopy(layer_1))
+
+    # what is this used for???
+    future_layer_1_delta = np.zeros(hidden_dim)
+
+    for position in range(6):
+        X = np.array([[num[-2-position]]])
+        layer_1 = layer_1_values[-1 - position]
+        prev_layer_1 = layer_1_values[-2 - position]
+
+        layer_2_delta = layer_2_deltas[-position - 1]
+
+
+        layer_1_delta = (future_layer_1_delta.dot(synapse_h.T) + \
+                         layer_2_delta.dot(synapse_1.T)) * sigmoid_otput_to_derivation(layer_1)
+
+
+        synapse_1_update += np.atleast_2d(layer_1).T.dot(layer_2_delta)
+        synapse_h_update += np.atleast_2d(prev_layer_1).T.dot(layer_1_delta)
+        synapse_0_update += np.atleast_2d(X).T.dot(layer_1_delta)
+
+        future_layer_1_delta=layer_1_delta
+
+    synapse_0+=synapse_0_update
+    synapse_1 += synapse_1_update
+    synapse_h += synapse_h_update
+
+
+    synapse_0_update *=0
+    synapse_1_update *=0
+    synapse_h_update *=0
+
+
+
+    if(i%1000==0):
+        print "Error: "+ str(overallError)
+        print "Pred: " + str(result)
+        print "true: " + str(num)
+
+   #         for index,x in enumerate(reversed(d)):
+ #           out+=x*pow(2,index)
+
+        #print str(a_int)+" + "+str(b_int)+" = "+str(out)
+
+
+
